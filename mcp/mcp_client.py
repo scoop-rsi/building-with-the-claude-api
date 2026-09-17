7176be06c269c7033dcca75a0efa9a18ab909a10
Frank LaFond <frank.lafond@teamsparq.com>
2026-09-17 11:58:58 -0600

    MCP - now have tools and resources

---
+++ mcp/mcp_client.py
@@ -0,0 +1,107 @@
+import asyncio
+import json
+import sys
+from contextlib import AsyncExitStack
+from typing import Any
+
+from mcp.client.stdio import stdio_client
+from pydantic import AnyUrl
+
+from mcp import ClientSession, StdioServerParameters, types
+
+
+class MCPClient:
+    def __init__(
+        self,
+        command: str,
+        args: list[str],
+        env: dict | None = None,
+    ):
+        self._command = command
+        self._args = args
+        self._env = env
+        self._session: ClientSession | None = None
+        self._exit_stack: AsyncExitStack = AsyncExitStack()
+
+    async def connect(self):
+        server_params = StdioServerParameters(
+            command=self._command,
+            args=self._args,
+            env=self._env,
+        )
+        stdio_transport = await self._exit_stack.enter_async_context(
+            stdio_client(server_params)
+        )
+        _stdio, _write = stdio_transport
+        self._session = await self._exit_stack.enter_async_context(
+            ClientSession(_stdio, _write)
+        )
+        await self._session.initialize()
+
+    @property
+    def session(self) -> ClientSession:
+        if self._session is None:
+            raise ConnectionError(
+                "Client session not initialized or cache not populated. Call connect_to_server first."
+            )
+        return self._session
+
+    async def list_tools(self) -> list[types.Tool]:
+        '''Return a list of tools defined by the MCP server'''
+        result = await self.session.list_tools()
+        return result.tools
+
+    async def call_tool(
+        self, tool_name: str, tool_input: dict) -> types.CallToolResult | None:
+        '''Call a particular tool and return the result'''
+        return await self.session.call_tool(tool_name, tool_input)
+
+    async def list_prompts(self) -> list[types.Prompt]:
+        # TODO: Return a list of prompts defined by the MCP server
+        return []
+
+    async def get_prompt(self, prompt_name, args: dict[str, str]):
+        # TODO: Get a particular prompt defined by the MCP server
+        return []
+
+    async def read_resource(self, uri: str) -> Any:
+        '''Read a resource, parse the contents and return it'''
+        result = await self.session.read_resource(AnyUrl(uri))
+        resource = result.contents[0]
+
+        if isinstance(resource, types.TextResourceContents):
+            if resource.mimeType == 'application/json':
+                return json.loads(resource.text)
+            return resource.text
+
+        raise ValueError('Resource contents was not of type "TextResourceContents"')
+
+    async def cleanup(self):
+        await self._exit_stack.aclose()
+        self._session = None
+
+    async def __aenter__(self):
+        '''Automatically triggered by entering "async with" block.'''
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        '''Automatically triggered by exiting "async with" block.'''
+        await self.cleanup()
+
+
+# For testing
+async def main():
+    async with MCPClient(
+        # If using Python without UV, update command to 'python' and remove "run" from args.
+        command="uv",
+        args=["run", "mcp_server.py"],
+    ) as _client:
+        result = await _client.list_tools()
+        print(result)
+
+
+if __name__ == "__main__":
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    asyncio.run(main())
---
+++ mcp/mcp_server.py
@@ -0,0 +1,62 @@
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+# A resource to read a document
+@mcp.tool(
+    name="read_doc_contents",
+    description="Read the contents of a document and return it as a string."
+)
+def read_document(
+    doc_id: str = Field(description="Id of the document to read")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+
+    return docs[doc_id]
+
+# A resource to edit a document
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the documents content with a new string."
+)
+def edit_document(
+    doc_id: str = Field(description="Id of the document that will be edited"),
+    old_str: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_str: str = Field(description="The new text to insert in place of the old text.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+
+@mcp.resource('docs://documents', mime_type='application/json')
+def list_docs() -> list[str]:
+    '''Resource to return all doc IDs'''
+    return list(docs.keys())
+
+# TODO: Write a resource to return the contents of a particular doc
+@mcp.resource('docs://documents/{doc_id}', mime_type='text/plain')
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise KeyError(f'No document with id "{doc_id}" exists')
+
+    return docs[doc_id]
+
+# TODO: Write a prompt to rewrite a doc in markdown format
+# TODO: Write a prompt to summarize a doc
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
